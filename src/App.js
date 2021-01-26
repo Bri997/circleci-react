82689a7405f4ff9a80363ae5d1b897ed6dfe9336
Bri997 <brian.musial@gmail.com>
2021-01-26 14:51:27 -0600

    changed text so it would pass test

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn CircleCI
+          Learn React
         </a>
+        <p>Hi</p>
       </header>
     </div>
   )
